6c101d9bb164428308150da4af157737e1459d7e
john gao <1649868243@qq.com>
2020-06-05 19:58:26 +0800

    add tensorboard log

--- reco/Config.py
+++ reco/Config.py
@@ -8,7 +8,7 @@ random_seed = 1111
 using_cuda = True
 keep_ratio = False
 gpu_id = '0'
-model_dir = './my_train_model'
+model_dir = './pretrain_train'
 # if data_worker > 0,some error raise on windows, why?
 data_worker = 0
 batch_size = 64
@@ -17,9 +17,10 @@ img_width = 168
 alphabet = alphabets.alphabet
 epoch = 20
 # my train dataset has 300,000 samples
+log_interval = 500
 display_interval = 20
-save_interval = 2000
-test_interval = 1500
+save_interval = 1000
+test_interval = 400
 test_disp = 20
 test_batch_num = 32
 lr = 0.0001
--- reco/train.py
+++ reco/train.py
@@ -11,52 +11,50 @@ import lib.utility
 from torch.autograd import Variable
 import Net.net as Net
 import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
 
 
 def val(net, da, criterion, max_iter=100):
     print('Start val')
-
-    for p in net.parameters():
-        p.requires_grad = False
-
     net.eval()
-    data_loader = torch.utils.data.DataLoader(
-        da, shuffle=True, batch_size=Config.batch_size, num_workers=int(Config.data_worker))
-    val_iter = iter(data_loader)
+    with torch.no_grad():
+        data_loader = torch.utils.data.DataLoader(
+            da, shuffle=True, batch_size=Config.batch_size, num_workers=int(Config.data_worker))
+        val_iter = iter(data_loader)
 
-    i = 0
-    n_correct = 0
-    loss_avg = lib.utility.averager()
+        i = 0
+        n_correct = 0
+        loss_avg = lib.utility.averager()
 
-    max_iter = min(max_iter, len(data_loader))
-    for i in range(max_iter):
-        data = val_iter.next()
-        i += 1
-        cpu_images, cpu_texts = data
-        batch_size = cpu_images.size(0)
-        lib.dataset.loadData(image, cpu_images)
-        t, l = converter.encode(cpu_texts)
-        lib.dataset.loadData(text, t)
-        lib.dataset.loadData(length, l)
+        max_iter = min(max_iter, len(data_loader))
+        for i in range(max_iter):
+            data = val_iter.next()
+            i += 1
+            cpu_images, cpu_texts = data
+            batch_size = cpu_images.size(0)
+            lib.dataset.loadData(image, cpu_images)
+            t, l = converter.encode(cpu_texts)
+            lib.dataset.loadData(text, t)
+            lib.dataset.loadData(length, l)
 
-        preds = net(image)
-        preds_size = torch.IntTensor([preds.size(0)] * batch_size)
-        cost = criterion(preds, text, preds_size, length) / batch_size
-        loss_avg.add(cost)
+            preds = net(image)
+            preds_size = torch.IntTensor([preds.size(0)] * batch_size)
+            cost = criterion(preds, text, preds_size, length) / batch_size
+            loss_avg.add(cost)
 
-        _, preds = preds.max(2)
-        preds = preds.transpose(1, 0).contiguous().view(-1)
-        sim_preds = converter.decode(preds.data, preds_size.data, raw=False)
-        list_1 = []
-        for i in cpu_texts:
-            list_1.append(i.decode('utf-8','strict'))
-
-        for pred, target in zip(sim_preds, list_1):
-            if pred == target:
-                n_correct += 1
+            _, preds = preds.max(2)
+            preds = preds.transpose(1, 0).contiguous().view(-1)
+            sim_preds = converter.decode(preds.data, preds_size.data, raw=False)
+            list_1 = []
+            for i in cpu_texts:
+                list_1.append(i.decode('utf-8', 'strict'))
+            for pred, target in zip(sim_preds, list_1):
+                if pred == target:
+                    n_correct += 1
 
     accuracy = n_correct / float(max_iter * Config.batch_size)
     print('Test loss: %f, accuray: %f' % (loss_avg.val(), accuracy))
+    return loss_avg.val(), accuracy
 
 
 def trainBatch(net, criterion, optimizer, train_iter):
@@ -67,11 +65,15 @@ def trainBatch(net, criterion, optimizer, train_iter):
     t, l = converter.encode(cpu_texts)
     lib.dataset.loadData(text, t)
     lib.dataset.loadData(length, l)
-
+    # print("------------------")
+    # print(image.device)
+    # print(text.device)
+    # print(type(text))
+    # print("------------------")
     preds = net(image)
 
-    preds_size = Variable(torch.IntTensor([preds.size(0)] * batch_size))  # preds.size(0)=w=22
-
+    preds_size = Variable(torch.IntTensor(
+        [preds.size(0)] * batch_size))  # preds.size(0)=w=22
     cost = criterion(preds, text, preds_size, length) / batch_size
     net.zero_grad()
     cost.backward()
@@ -90,19 +92,12 @@ if __name__ == '__main__':
     random.seed(Config.random_seed)
     np.random.seed(Config.random_seed)
     torch.manual_seed(Config.random_seed)
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = Config.gpu_id
-
-    cudnn.benchmark = True
-    if torch.cuda.is_available() and Config.using_cuda:
-        cuda = True
-        print('Using cuda')
-    else:
-        cuda = False
-        print('Using cpu mode')
-
+    device = torch.device("cuda:0" if torch.cuda.is_available()
+                          and Config.using_cuda else "cpu")
     train_dataset = lib.dataset.lmdbDataset(root=Config.train_data)
-    test_dataset = lib.dataset.lmdbDataset(root=Config.test_data, transform=lib.dataset.resizeNormalize((Config.img_width, Config.img_height)))
+    test_dataset = lib.dataset.lmdbDataset(
+        root=Config.test_data, transform=lib.dataset.resizeNormalize(
+            (Config.img_width, Config.img_height)))
     assert train_dataset
 
     # images will be resize to 32*100
@@ -114,7 +109,7 @@ if __name__ == '__main__':
         collate_fn=lib.dataset.alignCollate(imgH=Config.img_height, imgW=Config.img_width))
 
     n_class = len(Config.alphabet) + 1  # for python3
-    #n_class = len(Config.alphabet.decode('utf-8')) + 1  # for python2
+    # n_class = len(Config.alphabet.decode('utf-8')) + 1  # for python2
     print("alphabet class num is %s" % n_class)
     # 文字标签化
     converter = lib.convert.strLabelConverter(Config.alphabet)
@@ -124,37 +119,44 @@ if __name__ == '__main__':
     net = Net.CRNN(n_class)
     #  引用参数
     net.load_state_dict(torch.load("../weights/crnn/netCRNN_4_48000.pth"))
-
-    print("this is pretrain model parameters:")
-    print(net.state_dict())
-    image = torch.FloatTensor(Config.batch_size, 3, Config.img_height, Config.img_width)
+    # net.apply(lib.utility.weights_init)
+    # print("this is pretrain model parameters:")
+    # print(net.state_dict())
+    image = torch.FloatTensor(
+        Config.batch_size,
+        3,
+        Config.img_height,
+        Config.img_width)
     text = torch.IntTensor(Config.batch_size * 5)
     length = torch.IntTensor(Config.batch_size)
 
-    if cuda:
-        net.cuda()
-        image = image.cuda()
-        criterion = criterion.cuda()
+    net.to(device)
+    image = image.cuda()
 
-    image = Variable(image)
-    text = Variable(text)
-    length = Variable(length)
+    # for p in net.parameters():
+    #     print(p.device)
 
     loss_avg = lib.utility.averager()
 
     optimizer = optim.RMSprop(net.parameters(), lr=Config.lr)
-
+    writer = SummaryWriter('runs/crnn')
+    temp_input = torch.zeros((64, 1, 32, 168), device=device)
+    writer.add_graph(net, temp_input)
     for epoch in range(Config.epoch):
         train_iter = iter(train_loader)
         i = 0
         while i < len(train_loader):
-            for p in net.parameters():
-                p.requires_grad = True
             net.train()
 
             cost = trainBatch(net, criterion, optimizer, train_iter)
             loss_avg.add(cost)
             i += 1
+
+            if i % Config.log_interval == 0:
+                writer.add_scalar("training loss",
+                                  loss_avg.val(),
+                                  epoch * len(train_loader) + i
+                                  )
 
             if i % Config.display_interval == 0:
                 print('[%d/%d][%d/%d] Loss: %f' %
@@ -162,7 +164,11 @@ if __name__ == '__main__':
                 loss_avg.reset()
 
             if i % Config.test_interval == 0:
-                val(net, test_dataset, criterion)
+                loss, accuracy = val(net, test_dataset, criterion)
+                writer.add_scalar(
+                    'test/loss', loss, epoch * len(train_loader) + i)
+                writer.add_scalar(
+                    'test/accuracy', accuracy, epoch * len(train_loader) + i)
 
             # do checkpointing
             if i % Config.save_interval == 0:
